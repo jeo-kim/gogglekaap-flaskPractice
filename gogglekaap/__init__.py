c99b631a4f889ae35158cadf7afd019aebb0ad91
jeonghyeon <skjh0807s@snu.ac.kr>
2022-04-25 02:52:23 +0900

    flask_wtf:authForm

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -21,15 +21,41 @@ def create_app():
         return render_template('index.html')
 
     from gogglekaap.forms.auth_form import LoginForm, RegisterForm
-    @app.route('/auth/login')
+    @app.route('/auth/login', methods = ['GET', 'POST'])
     def login():
         form = LoginForm()
+        # POST method & validate OK
+        if form.validate_on_submit():
+            # Todo
+            # 1) 유저 조회
+            # 2) 존재하는 유저인지 체크
+            # 3) 패스워드 정합확인
+            # 4) 로그인 유지(세션)
+            user_id = form.data.get('user_id')
+            password = form.data.get('password')
+            return f'{user_id}, {password}'
+        else:
+            # Todo :error
+            pass
         return render_template('login.html', form=form)
 
-
-    @app.route('/auth/register')
+    @app.route('/auth/register',  methods = ['GET', 'POST'])
     def register():
         form = RegisterForm()
+        if form.validate_on_submit():
+            # Todo
+            # 1) 유저 조회
+            # 2) 유저 이미 존재하는지 체크
+            # 3) 없으면 유저 생성
+            # 4) 로그인 유지(세션)
+            user_id = form.data.get('user_id')
+            user_name = form.data.get('user_name')
+            password = form.data.get('password')
+            repassword = form.data.get('repassword')
+            return f'{user_id}, {user_name}, {password}, {repassword}'
+        else:
+            # Todo :error
+            pass
         return render_template('register.html', form=form)
 
     @app.route('/auth/logout')
@@ -39,6 +65,5 @@ def create_app():
     @app.errorhandler(404)
     def page_404(error):
         return render_template('/404.html'), 404
-
 
     return app
--- gogglekaap/forms/auth_form.py
+++ gogglekaap/forms/auth_form.py
@@ -1,7 +1,17 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
-    pass
+    user_id = StringField('User Id', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
 
-class RegisterForm(FlaskForm):
-    pass
+
+class RegisterForm(LoginForm):
+    password = PasswordField(
+        'Password', validators=[
+            DataRequired(),
+            EqualTo('repassword', message='Password Not Consistent!')])
+    repassword = PasswordField('Confirm Password', validators=[DataRequired()])
+
+    user_name = StringField('User Name', validators=[DataRequired()])
